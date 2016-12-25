3a90281789f46fd682a069f7bcb12a67e6e71ac7
user1 <jason.basanese@gmail.com>
2016-12-25 04:05:21 +0000

    fixed simultaneous drawing performance issues

--- js/socket.js
+++ js/socket.js
@@ -14,31 +14,24 @@ if(typeof G_vmlCanvasManager != 'undefined') {
 context = canvas.getContext("2d");
 
 var myId = Math.ceil(Math.random() * 10000) + "id" +Date.now();
-var drawState = {};
-var myColor = '#'+Math.random().toString(16).substr(-6); 
-
-function addDrawer(id, sendToId){
-		if(sendToId){
-    doTo([id], 'addDrawer', myId);
-		}else{
-		console.log('Drawer', id);
-		drawState[id] = {clickX : [], clickY : [], clickDrag : [], colors : []};
-		}
-}
 
 $('#canvas').mousedown(function(e){
   var mouseX = e.pageX - this.offsetLeft;
   var mouseY = e.pageY - this.offsetTop;
   paint = true;
-  doTo(['all'], 'addPaint', myId, e.pageX - this.offsetLeft, e.pageY - this.offsetTop, false, myColor);
-  doTo(['all'], 'redraw');
+  doTo(['all'], 'draw', myColor, mouseX -1, mouseY, mouseX, mouseY);
+	pastX = mouseX;
+	pastY = mouseY;
 });
 
 $('#canvas').mousemove(function(e){
+  var mouseX = e.pageX - this.offsetLeft;
+  var mouseY = e.pageY - this.offsetTop;
   if(paint){
-			doTo(['all'], 'addPaint', myId, e.pageX - this.offsetLeft, e.pageY - this.offsetTop, true, myColor);
-			doTo(['all'], 'redraw');
+  doTo(['all'], 'draw', myColor, pastX, pastY, mouseX, mouseY);
   }
+	pastX = mouseX;
+	pastY = mouseY;
 });
 
 $('#canvas').mouseup(function(e){
@@ -53,12 +46,12 @@ $('#canvas').mouseleave(function(e){
 //is because the deleteDrawing might not have been defined when the html is first read
 //due to the deleteDrawing being 
 //wrapped in a way that it is only called when page is loaded.
-$( "#clear" ).click(function(){doTo(['all'], 'deleteDrawing');});
+$( "#clear" ).click(function(){doTo(['all'], 'clearCanvas');});
 var paint;
 //This function stores the drawing data
 //if you store with dragging true redraw will interpet the two click points as a line
 //however if dragging is false redraw will interpret the x and y as a point
-var authenticatedFuncs = {deleteDrawing : deleteDrawing, log : log, addPaint : addPaint, redraw : redraw, addDrawer : addDrawer};
+var authenticatedFuncs = {clearCanvas: clearCanvas, log : log, draw : draw};
 function isForMe(target){
 		if(target.includes("all")){
 			return true;	
@@ -98,55 +91,28 @@ function doTo(who, func, ...params){
 		}
 }
 
-function addPaint(id, x, y, dragging, color)
-{
-  drawState[id].clickX.push(x);
-  drawState[id].clickY.push(y);
-  drawState[id].clickDrag.push(dragging);
-	drawState[id].colors.push(color);
-
-}
 
 //seperate function for clearing the canvas
 function clearCanvas(){
   context.clearRect(0, 0, context.canvas.width, context.canvas.height);
 }
 
-function deleteDrawing(){
-		for(var id in drawState){
-  		drawState[id].clickX.length = 0;
-  		drawState[id].clickY.length = 0;
-  		drawState[id].clickDrag.length = 0;
-			drawState[id].colors.length = 0;
-		}
-clearCanvas();
-}
+	var myColor = '#'+Math.random().toString(16).substr(-6); 
+	var pastX;
+	var pastY;
+
 //array object called drawings of objects with their keys as unique keys containing their colors and strokes
 //This is as close as one can get to a unique id without having to talk to other clients nor being able to have access to the server code.
-function redraw(){
-	clearCanvas();
+//using redraw with storing was getting performance issues, so I moved to a simple draw function
+function draw(color, startX, startY, endX, endY){
   context.lineJoin = "round";
   context.lineWidth = 5;
-	for(var id in drawState){		
-  		colors = drawState[id].colors;
-			clickX = drawState[id].clickX;
-			clickY = drawState[id].clickY;
-			clickDrag = drawState[id].clickDrag;
-  for(var i=0; i < clickX.length; i++) {		
-		context.strokeStyle = colors[i];
+		context.strokeStyle = color;
     context.beginPath();
-    if(clickDrag[i] && i){
-      context.moveTo(clickX[i-1], clickY[i-1]);
-     }else{
-				 //the reason for -1 here in clickX[i] -1 is that without it there would be no line to draw.
-				 //so basically instead of a point we are drawing a very small line
-       context.moveTo(clickX[i]-1, clickY[i]);
-     }
-     context.lineTo(clickX[i], clickY[i]);
-     context.closePath();
-     context.stroke();
-  }
-	}
+    context.moveTo(startX, startY);
+    context.lineTo(endX, endY);
+    context.closePath();
+    context.stroke();
 }
 
 var base = 'http://clicktime.herokuapp.com:80/rooms/';
@@ -163,11 +129,6 @@ socket.on('welcome', function () {
     // Connection is established, start using the socket
 		// Sends a console message to the other users in the room
     doTo(["others"], 'log','Another Drawer Has Joined');
-		//Makes the other users send their ids to my id.
-		//Those ids are used to set my drawers list
-    doTo(["others"], 'addDrawer', myId, 'To the given id');
-		//Adds my id to drawers for other users, and myself.
-		doTo(["all"], 'addDrawer', myId);
 });
    //Would have used 'function' type, but is not supported by provided server
 socket.on('message', function (data) {
